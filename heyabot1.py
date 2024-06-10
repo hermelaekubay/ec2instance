b7be8d9306caa436d23b23fee97af82d8684f668
Hermela Ekubay <hermelaekubay@gmail.com>
2024-06-10 11:35:27 -0700

    Adding changes

--- heyabot1.py
+++ heyabot1.py
@@ -2,13 +2,14 @@
 import discord
 import os
 import random
+from dotenv import load_dotenv
 from ec2_metadata import ec2_metadata
 
-print(ec2_metadata.region)
-print(ec2_metadata.instance_id)
+#print(ec2_metadata.region)
+#print(ec2_metadata.instance_id)
 
 # Creating a new instance of the Discord bot
-token = 'token'
+token = os.getenv("TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True  # Enable the message content intent
 intents.members = True  # Enable the members intent if needed
@@ -48,8 +49,11 @@ async def on_message(message):
             comment = [
                 "Love does not exist. Stop being delulu!",
                 "I love you too",
-                "I am not a human-being perhaps consider getting a boyfriend."
             ]
+        # Returning instance data for the last conditional statement.
+        elif user_message.lower() == "ec2 data": 
+            await message.channel.send(f"Your instance data is  {ec2_metadata.instance_id} Your EC2 Data: {ec2_metadata.region}") 
+
             await message.channel.send(random.choice(comment))
         return
 # Run the bot using the token
